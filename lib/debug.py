fc189cff1bb32ba8b4b897246f9430f574dbc2e1
Marc Katz <marc.f.katz@gmail.com>
2023-07-17 10:00:33 -0400

    Initial commit: create files, set up alembic db

---
+++ lib/debug.py
@@ -0,0 +1,15 @@
+import ipdb
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+#from <file> import <class>
+
+if __name__=='__main__':
+    engine = create_engine('sqlite:///factorio.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+
+    ipdb.set_trace()
+
+    session.close()
---
+++ lib/migrations/versions/208c4611f9c5_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 208c4611f9c5
+Revises: 
+Create Date: 2023-07-17 09:53:16.659345
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '208c4611f9c5'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
