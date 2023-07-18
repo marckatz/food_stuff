6564745a1dad1e1171baab4bacc93e54b0bc1c26
Marc Katz <marc.f.katz@gmail.com>
2023-07-18 17:11:50 -0400

    added metadata and convention to foodstuff.py

--- lib/foodstuff.py
+++ lib/foodstuff.py
@@ -2,8 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+convention = {
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s'
+}
+
 engine = create_engine('sqlite:///food_stuff.db')
 Session = sessionmaker( bind = engine )
 session = Session()
 
-Base = declarative_base()
+md = MetaData(naming_convention = convention)
+Base = declarative_base(metadata=md)
